83779b1e5636acc9792497c9b16796641f98adbc
Ira Ko <irako@Iras-MBP.home>
2017-06-09 10:29:59 -0400

    created selenium new tab workaround with vim

--- bb_reference.py
+++ bb_reference.py
@@ -7,19 +7,38 @@ Created on Thu Jun  8 22:21:47 2017
 """
 from NBA import sleeper
 
+import os
 from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
 import time
-from selenium import webdriver
-for i in sleeper(): 
-    driver = webdriver.Chrome('/Users/irako/Desktop/nba_rookies/chromedriver')  # Optional argument, if not specified will search path.
+
+
+executable_path = "/Users/irako/Desktop/nba_rookies/chromedriver"
+os.environ["webdriver.chrome.driver"] = executable_path
+
+chrome_options = Options()
+chrome_options.add_extension("/Users/irako/Desktop/nba_rookies/adb.crx")
+chrome_options.add_extension("/Users/irako/Desktop/nba_rookies/vim.crx")
+
+driver = webdriver.Chrome(executable_path=executable_path, chrome_options=chrome_options)
+
+for i in sleeper():     
     driver.get('http://www.basketball-reference.com/');
     search_box = driver.find_element_by_name('search')
     search_box.send_keys(i + Keys.ARROW_DOWN + Keys.RETURN)
-    driver.quit()
+    body = driver.find_element_by_tag_name('body')
+    body.send_keys('t' + 'K')
+    driver.switch_to_window(driver.window_handles[-1])
+
+    print(i)
+
+    
 
 
-
-
+    
+    
+    
     
